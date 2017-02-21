1d488914dc6f11abf8fbd2257562b553e14f25ce
JaviS10 <Javier_10.10@hotmail.com>
2017-02-21 13:39:11 -0600

    Update and rename Ejemplothree.js to EjemploThree.html

--- Ejemplothree.js
+++
@@ -1,7 +0,0 @@
-var escena = new THREE.Scene();
-var camara = new THREE.PerspectiveCamera();
-camara.position.z = 5;
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerHeight*.9, window.innerHeight*.9 );
-document.body.appendChild( renderer.domElement );
-renderer.render( escena, camara );
