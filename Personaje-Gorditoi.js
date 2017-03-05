734320e20e3785a74cb1f8f34d85cecf7152da5f
JaviS10 <Javier_10.10@hotmail.com>
2017-03-04 20:57:10 -0600

    Create Personaje-Gorditoi.js

---
+++ Personaje-Gorditoi.js
@@ -0,0 +1,42 @@
+var sombreroForma = new THREE.CylinderGeometry(5, 2, 2);
+var cabezaForma = new THREE.SphereGeometry(1.5);
+var cuerpoForma = new THREE.SphereGeometry(2.5);
+var soporteForma = new THREE.CylinderGeometry(1.5, 1.5, 10);
+var baseForma = new THREE.CylinderGeometry(5, 5, 1);
+
+soporteForma.rotateZ( Math.PI/4);
+soporteForma.translate(0, -5, 0);
+baseForma.translate(0,-10,0);
+cuerpoForma.translate(0, 2.5, 0);
+cabezaForma.translate(0, 4, 0);
+sombreroForma.translate(0 ,5, 0);
+
+var sombreroMalla = new THREE.Mesh(sombreroForma);
+var cabezaMalla = new THREE.Mesh(cabezaForma);
+var cuerpoMalla = new THREE.Mesh(cuerpoForma);
+var soporteMalla = new THREE.Mesh(soporteForma);
+var baseMalla = new THREE.Mesh(baseForma);
+
+var arbolForma = new THREE.Geometry();
+
+arbolForma.merge(sombreroMalla.geometry, sombreroMalla.matrix);
+arbolForma.merge(cabezaMalla.geometry, cabezaMalla.matrix);
+arbolForma.merge(cuerpoMalla.geometry, cuerpoMalla.matrix);
+arbolForma.merge(soporteMalla.geometry, soporteMalla.matrix);
+arbolForma.merge(baseMalla.geometry, baseMalla.matrix);
+
+var material = new THREE.MeshNormalMaterial();
+var arbolMalla = new THREE.Mesh(arbolForma, material);
+
+var escena = new THREE.Scene();
+escena.add(arbolMalla);
+//escena.add(troncoMalla);
+//escena.add(esferaMalla);
+
+var camara = new THREE.PerspectiveCamera();
+camara.position.z = 50;
+
+renderizador = new THREE.WebGLRenderer();
+renderizador.setSize( window.innerHeight*.95, window.innerHeight*.95 );
+document.body.appendChild( renderizador.domElement );
+renderizador.render( escena, camara );
