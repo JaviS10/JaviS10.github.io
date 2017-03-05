ac110f3e0c85f12d75353fcb27f01ed08554b44b
JaviS10 <Javier_10.10@hotmail.com>
2017-03-04 19:33:30 -0600

    Update ejemplo-union.js

--- ejemplo-union.js
+++ ejemplo-union.js
@@ -5,6 +5,14 @@ esferaForma.translate(0,1,0);
 var troncoMalla = new THREE.Mesh(troncoForma);
 var esferaMalla = new THREE.Mesh(esferaForma);
 
+var arbolForma = new THREE.Geometry();
+
+arbolForma.merge(troncoMalla.geometry, troncoMalla.matrix);
+arbolForma.merge(esferaMalla.geometry, esferaMalla.matrix);
+
+var material = new THREE.MeshNormalMaterial();
+var arbolMalla = new THREE.Mesh(arbolForma, material);
+
 var escena = new THREE.Scene();
 escena.add(arbolMalla);
 
@@ -15,4 +23,3 @@ renderizador = new THREE.WebGLRenderer();
 renderizador.setSize( window.innerHeight*.95, window.innerHeight*.95 );
 document.body.appendChild( renderizador.domElement );
 renderizador.render( escena, camara );
-
