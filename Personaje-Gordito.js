eaeea8baec075cc194d25eb7f6817c634a40fd8c
JaviS10 <Javier_10.10@hotmail.com>
2017-03-04 20:29:01 -0600

    Update Personaje-Gordito.js

--- Personaje-Gordito.js
+++ Personaje-Gordito.js
@@ -11,6 +11,35 @@ cuerpoForma.translate(0, 2.5, 0);
 cabezaForma.translate(0, 4, 0);
 sombreroForma.translate(0 ,5, 0);
 
+var sombreroMalla = new THREE.Mesh(sombreroForma);
+var cabezaMalla = new THREE.Mesh(cabezaForma);
+var cuerpoMalla = new THREE.Mesh(cuerpoForma);
+var soporteMalla = new THREE.Mesh(soporteForma);
+var baseMalla = new THREE.Mesh(baseForma);
+
+var arbolForma = new THREE.Geometry();
+
+arbolForma.merge(sombreroMalla.geometry, sombreroMalla.matrix);
+arbolForma.merge(cabezaMalla.geometry, cabezaMalla.matrix);
+arbolForma.merge(cuerpoMalla.geometry, cuerpoMalla.matrix);
+arbolForma.merge(soporteMalla.geometry, soporteMalla.matrix);
+arbolForma.merge(baseMalla.geometry, baseMalla.matrix);
+
+var material = new THREE.MeshNormalMaterial();
+var arbolMalla = new THREE.Mesh(arbolForma, material);
+
+var escena = new THREE.Scene();
+escena.add(arbolMalla);
+//escena.add(troncoMalla);
+//escena.add(esferaMalla);
+
+var camara = new THREE.PerspectiveCamera();
+camara.position.z = 50;
+
+renderizador = new THREE.WebGLRenderer();
+renderizador.setSize( window.innerHeight*.95, window.innerHeight*.95 );
+document.body.appendChild( renderizador.domElement );
+renderizador.render( escena, camara );
 
 
 
