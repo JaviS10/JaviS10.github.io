0a0fbbbfaeae3754ba80d4693642ea53d7d3196f
JaviS10 <Javier_10.10@hotmail.com>
2017-03-05 09:17:06 -0600

    Update Personaje-Gorditoi.js

--- Personaje-Gorditoi.js
+++ Personaje-Gorditoi.js
@@ -5,11 +5,11 @@ var soporteForma = new THREE.CylinderGeometry(1.5, 1.5, 5);
 var baseForma = new THREE.CylinderGeometry(5, 5, 1);
 
 //soporteForma.rotateZ( Math.PI/4);
-soporteForma.translate(0, -8, 0);
+soporteForma.translate(0, -4, 0);
 baseForma.translate(0,-8,0);
-cuerpoForma.translate(0, 2.5, 0);
-cabezaForma.translate(0, 3, 0);
-sombreroForma.translate(0 ,7, 0);
+cuerpoForma.translate(0, 1.5, 0);
+cabezaForma.translate(0, 4, 0);
+sombreroForma.translate(0 ,10, 0);
 
 var sombreroMalla = new THREE.Mesh(sombreroForma);
 var cabezaMalla = new THREE.Mesh(cabezaForma);
