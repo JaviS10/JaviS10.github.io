c3a2e6632eb17a982065915a36b76a8a4fd03541
JaviS10 <Javier_10.10@hotmail.com>
2017-03-20 19:00:22 -0600

    Update cameras.js

--- practica2/cameras.js
+++ practica2/cameras.js
@@ -1,20 +1,3 @@
-var escena= new THREE.Scene();
-
-//var camara= new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
-//camara.position.set(0,-200,100);
-//camara.lookAt(escena.position);
-
-var camara= new THREE.OrthographicCamera();
-camara.left=window.innerWidth/-2;
-camara.right=window.innerWidth/2;
-camara.top=window.innerHeight/2;
-camara.bottom=window.innerHeight/-2;
-camara.near=0.1;
-camara.far=1000;
-camara.updateProjectionMatrix();
-camara.position.set(0,-200,100);
-camara.lookAt(escena.position);
-
 //-----Personaje Gordito.
 var sombreroForma1 = new THREE.CylinderGeometry(2, 5, 2);
 var cabezaForma1 = new THREE.SphereGeometry(3);
@@ -89,6 +72,22 @@ escena.add(Piso);
   }
 }  
  //Piso.position.x=20;
+var escena= new THREE.Scene();
+
+//var camara= new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
+//camara.position.set(0,-200,100);
+//camara.lookAt(escena.position);
+
+var camara= new THREE.OrthographicCamera();
+camara.left=window.innerWidth/-2;
+camara.right=window.innerWidth/2;
+camara.top=window.innerHeight/2;
+camara.bottom=window.innerHeight/-2;
+camara.near=0.1;
+camara.far=1000;
+camara.updateProjectionMatrix();
+camara.position.set(0,-200,100);
+camara.lookAt(escena.position);
 
 var renderizador= new THREE.WebGLRenderer();
 renderizador.setSize(window.innerWidth,window.innerHeight);
